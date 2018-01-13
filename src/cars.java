7bab7f707b9cd13e78d0aff2f5bf009c614046c7
agatito <agatito@yopmail.com>
2018-01-13 16:13:50 +0100

    zmiana wyświetlania z println na printf

--- src/cars.java
+++ src/cars.java
@@ -4,10 +4,9 @@ class Cars {
         Car car2 = new Car("Opel", "Vectra", 2004, 1.8);
 
         System.out.println("Samochód 1:");
-        System.out.println("Marka: " + car1.brand + ", model: " + car1.model +
-                ", rocznik: " + car1.year + ", pojemność: " + car1.capacity);
+        System.out.printf("Marka: %s, model: %s, rocznik: %d, pojemność: %d", car1.brand, car1.model, car1.year, car1.capacity);
+        System.out.println("");
         System.out.println("Samochód 2:");
-        System.out.println("Marka: " + car2.brand + ", model: " + car2.model +
-                ", rocznik: " + car2.year + ", pojemność: " + car2.capacity);
+        System.out.printf("Marka: %s, model: %s, rocznik: %d, pojemność: %d", car2.brand, car2.model, car2.year, car2.capacity);
     }
 }
